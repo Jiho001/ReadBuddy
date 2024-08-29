c482221cfc1f96b305f764709466e2086ddc2cee
Jiho <ruddbsghdus@naver.com>
2024-08-29 23:57:58 +0900

    국중도 api 개발중

--- src/main/java/com/readBuddy/controller/api/HomeApiController.java
+++ src/main/java/com/readBuddy/controller/api/HomeApiController.java
@@ -4,6 +4,7 @@ import com.readBuddy.domain.UserBook;
 import com.readBuddy.dto.BookDto;
 import com.readBuddy.dto.UserRequestDto;
 import com.readBuddy.dto.UserResponseDto;
+import com.readBuddy.service.BookApiService;
 import com.readBuddy.service.BookService;
 import com.readBuddy.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeApiController {
     private final UserService userService;
     private final BookService bookService;
+    private final BookApiService bookApiService;
 
     @PostMapping("/join/members")
     public UserResponseDto joinMember(UserRequestDto request) {
@@ -35,9 +37,8 @@ public class HomeApiController {
         return new BookDto(bookId);
     }
 
-
-
-
-
-
+    @PostMapping("/recomm/book")
+    public String recommendBook() {
+        return bookApiService.recommendBook();
+    }
 }
--- src/main/java/com/readBuddy/service/BookApiService.java
+++ src/main/java/com/readBuddy/service/BookApiService.java
@@ -4,6 +4,8 @@ import com.readBuddy.dto.BookApiDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
 @Service
@@ -11,13 +13,21 @@ public class BookApiService {
     @Value("${api.key}")
     private String apiKey;
 
-    public void recommendBook() {
-        RestClient restClient = RestClient.create();
-        BookApiDto recomBook = restClient.post()
-                .uri("https://nl.go.kr/NL/search/openApi/saseoApi.do")
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .body(BookApiDto.class);
-    }
+    public String recommendBook() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl("https://nl.go.kr/NL/search/openApi/saseoApi.do")
+                .build();
 
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("key", apiKey);
+        formData.add("drCode", "11");
+
+        String response = restClient.post()
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(formData)
+                .retrieve()
+                .body(String.class);
+
+        return response;
+    }
 }
